871c12e0cf8fcb48365f32e0130d22e29457c693
alex-yellow <72997598+alex-yellow@users.noreply.github.com>
2022-04-22 19:51:21 +0300

    Add files via upload

---
+++ LoginMVC/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using LoginMVC.Models;
+using LoginMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Account account)
+        {
+            if(account.Username.Equals("abc") && account.Password.Equals("123"))
+            {
+                Session["username"] = account.Username;
+                return View("Welcome");
+            }
+            else
+            {
+                ViewBag.Error = "Аккаунт не найден";
+                return View("Index");
+            }
+        }
+        public ActionResult Logout()
+        {
+            Session.Remove("username");
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ LoginMVC/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LoginMVC.Models
+{
+    public class Account
+    {
+        [Display(Name = "Имя")]
+        public string Username { get; set; }
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+    }
+}
